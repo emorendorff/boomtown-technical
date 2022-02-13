c7c2a9c5b5a6ee4a4ae4aa2090c1b377f1293eef
Em Orendorff <em.orendorff@gmail.com>
2022-02-13 15:00:26 -0500

    Filter top level key/values to set url paths

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react'
 
 export const Home = () => {
   const [baseApi, setBaseApi] = useState([])
+  const [paths, setPaths] = useState([])
   
   // Fetch top level api and set state 
   const getTopLevelData = async () => {
@@ -10,25 +11,33 @@ export const Home = () => {
       const response = await fetch(url);
       let baseApi = await response.json();
       //convert json into array to be able to iterate through
-      baseApi = Object.entries(baseApi).map((e) => ( { [e[0]]: e[1] } ))
+      // baseApi = Object.entries(baseApi).map((e) => ( { [e[0]]: e[1] } ))
       setBaseApi(baseApi)
-      return baseApi
+      filterPaths()
   } catch (error) {
       console.log("error", error);
     }
   } 
+  
+  const filterPaths = () => {
+    let paths  = []
+    paths.push(baseApi.repos_url, baseApi.events_url, baseApi.hooks_url, baseApi.issues_url, baseApi.members_url, baseApi.public_members_url)
+    setPaths(paths)
+  }
 
 
   // ideal world I would turn json object into an array, then create an array that just include the 
   //_url path. then 
 
   // const mapDataUrls = () => {
-  //   const urlPaths = baseApi.includes('_url')
-  //   console.log(urlPaths)
+  //   const urlPaths = baseApi.filter(url => url.includes('BoomTownROI/'))
+    
   // }
   
   useEffect(() => {
     getTopLevelData()
+    // console.log(filterPaths())
+    // mapDataUrls()
     // console.log(baseApi)
     // arrayifyData()
 
