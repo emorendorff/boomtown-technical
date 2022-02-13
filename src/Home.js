263e24b5dbb2abf5c881bf29e97621d86dfc09ae
Em Orendorff <em.orendorff@gmail.com>
2022-02-13 13:09:29 -0500

    Convert json response to array

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,38 @@
 import {useState, useEffect} from 'react'
 
 export const Home = () => {
-  useEffect(() => {
+  const [baseApi, setBaseApi] = useState([])
+  
+  // Fetch top level api and set state 
+  const getTopLevelData = async () => {
     const url = "https://api.github.com/orgs/BoomTownROI"
-    
-    const fetchData = async () => {
-      try {
-          const response = await fetch(url);
-          const json = await response.json();
-          console.log(json);
-      } catch (error) {
-          console.log("error", error);
-      }
-  };
-    fetchData()
+    try {
+      const response = await fetch(url);
+      let baseApi = await response.json();
+      //convert json into array to be able to iterate through
+      baseApi = Object.entries(baseApi).map((e) => ( { [e[0]]: e[1] } ))
+      setBaseApi(baseApi)
+      return baseApi
+  } catch (error) {
+      console.log("error", error);
+    }
+  } 
+
+
+  // ideal world I would turn json object into an array, then create an array that just include the 
+  //_url path. then 
+
+  // const mapDataUrls = () => {
+  //   const urlPaths = baseApi.includes('_url')
+  //   console.log(urlPaths)
+  // }
+  
+  useEffect(() => {
+    getTopLevelData()
+    // console.log(baseApi)
+    // arrayifyData()
+
+  
   }, []);
 
 
