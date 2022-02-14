dcae2bab095b62c89b365ca052205101080a44f6
Em Orendorff <em.orendorff@gmail.com>
2022-02-14 09:03:02 -0500

    Fetch repo and events data

--- src/Home.js
+++ src/Home.js
@@ -2,40 +2,76 @@ import {useState, useEffect} from 'react'
 
 export const Home = () => {
   const [baseApi, setBaseApi] = useState([])
+  const[reposApi, setReposApi] = useState([])
+  const [eventsApi, setEventsApi] = useState([])
   const [paths, setPaths] = useState([])
+  // let url = "https://api.github.com/orgs/BoomTownROI"
+  let url = "https://api.github.com/orgs/BoomTownROI"
+  const repoUrl = url + '/repos'
+  const eventsUrl = url + '/events'
   
-  // Fetch top level api and set state 
-  const getTopLevelData = async () => {
-    const url = "https://api.github.com/orgs/BoomTownROI"
+  const getAllData = async () => {
     try {
-      const response = await fetch(url);
-      let baseApi = await response.json();
-      //convert json into array to be able to iterate through
-      // baseApi = Object.entries(baseApi).map((e) => ( { [e[0]]: e[1] } ))
-      setBaseApi(baseApi)
-      filterPaths()
-  } catch (error) {
-      console.log("error", error);
+        let baseResponse = await fetch(url)
+        let baseApi = await baseResponse.json()
+        setBaseApi(baseApi)
+        console.log(baseApi)
+    } catch (error) {
+      console.log('error', error)
     }
-  } 
-  
-  const filterPaths = () => {
-    let paths  = []
-    paths.push(baseApi.repos_url, baseApi.events_url, baseApi.hooks_url, baseApi.issues_url, baseApi.members_url, baseApi.public_members_url)
-    setPaths(paths)
   }
+
+  const getReposData = async () => {
+    try {
+        let repoResponse = await fetch(repoUrl)
+        let reposApi = await repoResponse.json()
+        setReposApi(reposApi)
+        console.log(reposApi)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  const getEventsData = async () => {
+    try {
+        let eventsResponse = await fetch(eventsUrl)
+        let eventsApi = await eventsResponse.json()
+        setEventsApi(eventsApi)
+        console.log(eventsApi)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+  
+  //     // baseApi = Object.entries(baseApi).map((e) => ( { [e[0]]: e[1] } ))
+
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch((`https://api.github.com/orgs/BoomTownROI/${paths[i]}`))
+  //     checkForError(response)
+  //     console.log(response)
+  //   } catch (error) {
+  //     console.log('error', error)
+  //   }
+  // }
+
+  // const checkForError = (response) => {
+  //   if (!response.ok) {
+  //     const message = `An error has occured: ${response.status}`;
+  //     throw new Error(message)
+  //   } 
+  // }
 
 
   // ideal world I would turn json object into an array, then create an array that just include the 
   //_url path. then 
-
-  // const mapDataUrls = () => {
-  //   const urlPaths = baseApi.filter(url => url.includes('BoomTownROI/'))
-    
-  // }
   
   useEffect(() => {
-    getTopLevelData()
+    getAllData()
+    getReposData()
+    getEventsData()
+    // getTopLevelData()
+    // fetchPaths()
     // console.log(filterPaths())
     // mapDataUrls()
     // console.log(baseApi)
